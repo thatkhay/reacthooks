1d6d51df8df09b83177bd1cb8c8867aa213c5f45
thatkhay <johnkingsleyegeonu@gmail.com>
2023-06-22 08:51:24 +0100

    useeffect

--- reacthooks-practice/Useeffect/useeffect/src/App.js
+++ reacthooks-practice/Useeffect/useeffect/src/App.js
@@ -9,14 +9,15 @@ useEffect(() =>{
 console.log('onmount');
 }, [resourceType])
   return (
-    <>
+    <div style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column'}}>
     <div className="App">
-       <button onClick={() => setResourceType('post')} >post</button>
-       <button onClick={() => setResourceType('users')} >users</button>
-       <button onClick={() => setResourceType('comment')} >comment</button>
+    <h5>This use effect hook is used to perform side effects when the state chaages, if for exammple 'post' is clicked once and cicked agaib with out clicking on any other button before that 'onmount' is logeed once in the console but if clicked twice on mont is logged only the first time. this shows use effect runs only when the stste attached id changed.</h5>
+       <button style={{height: '3rem', width:'5rem', backgroundColor: 'red'}}  onClick={() => setResourceType('post')} >post</button>
+       <button style={{height: '3rem', width:'5rem', backgroundColor: 'blue'}} onClick={() => setResourceType('users')} >users</button>
+       <button style={{height: '3rem', width:'5rem', backgroundColor: 'green'}} onClick={() => setResourceType('comment')} >comment</button>
     </div>
     <h1>{resourceType}</h1>
-    </>
+    </div>
     
   );
 }
